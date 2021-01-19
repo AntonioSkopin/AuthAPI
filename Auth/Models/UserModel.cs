0e2ae23167c5784187bbc3dbedcccebf98ec7018
AntonioSkopin <askopin@outlook.com>
2021-01-19 16:50:21 +0100

    Added Authorization functionality

---
+++ Auth/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthAPI.Auth.Models
+{
+    public class UserModel
+    {
+        public Guid Gd { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+    }
+}
